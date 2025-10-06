c775826a550538cd2b444eff40e8cfec8b39e709
ffeaugas <feaugas.francis@gmail.com>
2025-10-06 20:04:08 +0200

    fix: deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,11 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/lueur-studio/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
   },
-  publicDir: process.env.NODE_ENV === 'production' ? 'lueur-studio/public' : 'public',
+  publicDir: 'public',
   assetsInclude: ['**/*.glb', '**/*.otf', '**/*.ttf'],
 })
