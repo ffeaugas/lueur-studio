df6658de69bd40d822a32755401b5d82eb99f7fc
ffeaugas <feaugas.francis@gmail.com>
2025-10-06 22:56:33 +0200

    fix: deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 export default defineConfig({
-  base: '/lueur-studio/',
+  base: process.env.NODE_ENV === 'production' ? '/lueur-studio/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
